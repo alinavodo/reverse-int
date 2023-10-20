51a4d95459bfa53cd363ff3f3db9eab6f3caef5a
alinavodo <alinavodo@gmail.com>
2023-10-20 14:42:41 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n % 10 === 0  &&  n < 0) {
+        n = n / 10 * (-1);
+        return Array.from(String(n), Number).reverse().join('');
+    } if( n % 10 === 0 ) {
+        n = n / 10;
+        return Array.from(String(n), Number).reverse().join('');
+    } if ( n > 0) { 
+        return Array.from(String(n), Number).reverse().join('');
+    } if( n < 0) {
+        n = n * (-1);
+       return Array.from(String(n), Number).reverse().join('');
+    }  
 }
